2eb99f24f12af269eda5a624faeac6a6b00ee053
beepsin3 <bryanelian.espinoza@gmail.com>
2025-05-20 09:58:53 -0700

    Clock + fps limit added

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
