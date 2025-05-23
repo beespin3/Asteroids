659f5aaa144ecda9ed4c60d8e48f98e6a311d12d
beepsin3 <bryanelian.espinoza@gmail.com>
2025-05-23 10:54:59 -0700

    added groups, player movement completed

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
+    
     
     while True:
         for event in pygame.event.get():
@@ -16,8 +22,12 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        
+        for d in drawable:
+            d.draw(screen)
+        
+        updatable.update(dt)
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
