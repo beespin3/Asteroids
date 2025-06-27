cd1d0de6390db8765c42213a64302d8bd0eed4cd
beepsin3 <bryanelian.espinoza@gmail.com>
2025-06-27 15:07:08 -0700

    Shots and asteroids now dissappear on collision

--- main.py
+++ main.py
@@ -44,6 +44,13 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+                
+        # checking bullet collision with asteroids
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
         
         pygame.display.flip()
         
